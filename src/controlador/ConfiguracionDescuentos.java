74e3f3edb985d30d28ebbc666d54f658c0811367
JDTorresC <dtorres@pibox.app>
2024-11-09 19:41:01 -0500

    Avances controlador javadoc

--- src/controlador/ConfiguracionDescuentos.java
+++ src/controlador/ConfiguracionDescuentos.java
@@ -3,9 +3,17 @@ package controlador;
 import java.util.Arrays;
 import java.util.List;
 
+//Configura los descuentos aplicables para clientes afiliados y no afiliados.
+//Define categorías de descuento, porcentajes y compra mínima.
+
 public class ConfiguracionDescuentos {
+	//Categorías de productos con descuento para no afiliados.
+	
 	public static final List<String> CATEGORIAS_DESCUENTO_NO_AFILIADO = Arrays.asList("BEBIDAS", "ASEO");
+	//Porcentaje de descuento para afiliados.
 	public static final int PORCENTAJE_DESCUENTO_AFILIADOS = 5;
+	//Porcentaje de descuento para no afiliados.
 	public static final int PORCENTAJE_DESCUENTO_NO_AFILIADOS = 5;
+	//Valor mínimo de compra para descuento de no afiliados.
 	public static final float VALOR_COMPRA_MINIMA_DESCUENTO_NO_AFILIADOS = 80000;
 }
